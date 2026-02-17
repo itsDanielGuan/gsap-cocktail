0dcef3fe0b89b2f089ad30aad85cfc3e88bd7541
Daniel Guan <danielguan23@gmail.com>
2026-02-17 14:48:41 +0800

    cocktails menu animated section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import gsap from 'gsap';
 import {ScrollTrigger, SplitText} from "gsap/all";
 import Navbar from './components/navbar';
 import Hero from './components/hero';
+import Cocktails from './components/Cocktails';
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -11,6 +12,7 @@ const App = () => {
     <main>
       <Navbar />
       <Hero />
+      <Cocktails />
       <div className='h-dvh bg-black'>
 
       </div>
